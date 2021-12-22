c280af55fe865b4c5fcfba0b0290a0eef0018403
Darren Lawton <darren@darrenlawton.com>
2021-12-22 13:05:39 +1100

    dynamic questions

--- Day17_Quiz/main.py
+++ Day17_Quiz/main.py
@@ -1,15 +1,23 @@
 from question_model import Question
-from data import question_data
 from quiz_brain import QuizBrain
+import requests
 
-def CreateQuestionBank():
+def CreateQuestionBank(n_questions = 10, difficulty = 'Medium'):
+	url = "https://opentdb.com/api.php?amount="+n_questions+"&category=9&difficulty="+difficulty.lower()+"&type=boolean"
+	source = requests.get(url).json()
+
 	question_bank = []
-	for qdict in question_data:
-		question_bank.append(Question(qdict["text"], qdict["answer"]))
+	for qdict in source["results"]:
+		question_bank.append(Question(qdict["question"], qdict["correct_answer"]))
 	return question_bank
 
-quiz = QuizBrain(CreateQuestionBank())
-while quiz.still_has_questions():
-	quiz.next_question()
+if __name__ == "__main__":
+	# Get quiz questions based on user inputs
+	number_of_questions = input("How many questions would you like? ")
+	difficulty = input("Select your level of difficulty ('Easy','Medium','Hard'): ")
 
-print(f"Your final score is {quiz.score}/{quiz.question_number}")
+	quiz = QuizBrain(CreateQuestionBank(number_of_questions, difficulty))
+	while quiz.still_has_questions():
+		quiz.next_question()
+
+	print(f"Your final score is {quiz.score}/{quiz.question_number}")
